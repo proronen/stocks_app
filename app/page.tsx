e581f02fe68a5a2a6a9caf68417be6c6a58dc123
proronen <proronen@gmail.com>
2026-01-18 21:29:01 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">page</div>
+      <Button></Button>
+    </>
+  );
+};
+
+export default page;
