4c080a45a24238865d4a8e0863a52326f39993bb
proronen <proronen@gmail.com>
2026-01-19 01:26:20 +0200

    remove unused constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -325,9 +325,6 @@ export const POPULAR_STOCK_SYMBOLS = [
     'SE',
 ];
 
-export const NO_MARKET_NEWS =
-    '<p class="mobile-text" style="margin:0 0 20px 0;font-size:16px;line-height:1.6;color:#4b5563;">No market news available today. Please check back tomorrow.</p>';
-
 export const WATCHLIST_TABLE_HEADER = [
     'Company',
     'Symbol',
