5764443723161b9ff90bbee8484a93abc467d432
proronen <proronen@gmail.com>
2026-01-19 01:21:31 +0200

    small fix

--- lib/constants.ts
+++ lib/constants.ts
@@ -113,7 +113,7 @@ export const HEATMAP_WIDGET_CONFIG = {
     hasSymbolTooltip: true,
     isMonoSize: false,
     width: '100%',
-    height: '600',
+    height: 600,
 };
 
 export const TOP_STORIES_WIDGET_CONFIG = {
@@ -124,7 +124,7 @@ export const TOP_STORIES_WIDGET_CONFIG = {
     locale: 'en',
     market: 'stock',
     width: '100%',
-    height: '600',
+    height: 600,
 };
 
 export const MARKET_DATA_WIDGET_CONFIG = {
